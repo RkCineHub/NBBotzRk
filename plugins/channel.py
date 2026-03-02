c4cf2df812d627c5d52e1dca2d33eaee75e979bc
Rk Cine Hub <rkcinehub@gmail.com>
2026-03-03 01:28:07 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -25,7 +25,7 @@ DEFAULT_IMAGE_URL = "https://te.legra.ph/file/88d845b4f8a024a71465d.jpg"
 SILENTX_PREMIUM_UPDATE = """
 <blockquote>🎬 𝕻ℝ𝔼𝕄𝕀𝕌𝕄 𝕄𝕆𝕍𝕀𝔼 𝕌ℙ𝔻𝔸𝕋𝔼 🎥</blockquote>
 
-<b><u>{}</u></b> <code>#{}</code>
+ᴛɪᴛʟᴇ » <code>{}</code> <b>#{}</b>
 
 <code>━━━━━━━━━━━━━━━━━━</code>
 <b>🔈 Audio</b>: {}
@@ -38,7 +38,7 @@ SILENTX_PREMIUM_UPDATE = """
 <b>🏷️ Genres</b>: {}
 <code>━━━━━━━━━━━━━━━━━━</code>
 
-<b>⚡ Powered By @SilentXBotz</b>
+<b>⚡ Powered By @RkCineHub</b>
 """
 
 notified_movies = set()
