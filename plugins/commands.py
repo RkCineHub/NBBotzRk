a2a7d0d144735d56ff48bb908547a8d7f8f3e89a
Rk Cine Hub <rkcinehub@gmail.com>
2026-03-03 01:19:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -95,13 +95,10 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('+ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ +', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🧧 ᴛʀᴇɴᴅɪɴɢ', callback_data="topsearch"),
-                    InlineKeyboardButton('🎟️ ᴜᴘɢʀᴀᴅᴇ', callback_data="premium"),
-                ],[
                     InlineKeyboardButton('♻️ ᴅᴍᴄᴀ', callback_data='disclaimer'),
                     InlineKeyboardButton('👤 ᴀʙᴏᴜᴛ', callback_data='me')
                 ],[
-                    InlineKeyboardButton('🚫 ᴇᴀʀɴ ᴍᴏɴᴇʏ ᴡɪᴛʜ ʙᴏᴛ 🚫', callback_data="earn")
+                    InlineKeyboardButton('🔍Sᴇᴀʀᴄʜ Mᴏᴠɪᴇs ᴏʀ Sᴇʀɪᴇs🔎', url=GRP_LNK)
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
